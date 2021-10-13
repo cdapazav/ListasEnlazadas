2de49537e13e67d3cb19b68b6a45d5e42bae08f9
cdapazav <cristofer16apaza@gmail.com>
2021-10-13 17:00:45 -0400

    Agregar archivos de proyecto.

---
+++ ListasEnlazadas/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListasEnlazadas
+{
+    public partial class Form1 : Form
+    {
+        ListasSimplementeEnlazadas lse = new ListasSimplementeEnlazadas();
+        public void MostrarPila()
+        {
+            listView1.Clear();
+            Nodo p;
+            p = lse.tope;
+            while(p!=null)
+            {
+                listView1.Items.Add((p.Informacion).ToString());
+                p = p.Enlace;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lse.insertarPila(Int32.Parse(textBox1.Text));
+            textBox1.Clear();
+            textBox1.Focus();
+            MostrarPila();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lse.EliminarPila();
+            MostrarPila();
+        }
+    }
+}
---
+++ ListasEnlazadas/ListasSimplementeEnlazadas.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListasEnlazadas
+{
+    //implementando con pilas
+    class ListasSimplementeEnlazadas
+    {
+        public Nodo tope = null;
+
+        private bool VaciaPila()
+        {
+            if (tope == null)
+                return true;
+            else
+                return false;
+        }
+
+        public void insertarPila(int elemento )
+        {
+            Nodo p;
+            if(VaciaPila())
+            {
+                tope = new Nodo();
+                tope.Informacion = elemento;
+                tope.Enlace = null;
+            }
+            else
+            {
+                p = new Nodo();
+                p.Informacion = elemento;
+                p.Enlace = tope;
+                tope = p;
+            }
+        }
+
+        public void EliminarPila()
+        {
+            Nodo p;
+            if (VaciaPila())
+                MessageBox.Show("La pila esta vacia, no se puede eliminar elementos");
+            else
+            {
+                MessageBox.Show("Elemento eliminado es: "+tope.Informacion);
+                p = tope;
+                tope = tope.Enlace;
+                p.Enlace = null;
+                p = null;
+            }
+        }
+    }
+}
